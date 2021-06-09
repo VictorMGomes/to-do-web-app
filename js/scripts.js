5ecd30d0519f1f3d409f91b8023535d299edda5c
Victor Gomes <victorvhmgomes@gmail.com>
2021-06-08 23:29:09 -0300

    Event to add a new task created

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,28 @@
 let  inputNewTask = document.querySelector("#add-task");
 let  btnNewTask = document.querySelector("#add-btn");
 let taskList = document.querySelector("#task-list");
-  function escrever() {
-    console.log(inputNewTask.value);  
-  }
-  btnNewTask.addEventListener("click", escrever);
+
+//Function to create a new task
+  function setNewTask() {
+    if (!inputNewTask.value == "") {
+      let taskItem= document.createElement('div');
+      taskItem.innerHTML=`<div class="task-item">
+                        <li>${inputNewTask.value}</li>
+                        <div class="group-btn">
+                        <button id="edit-btn"><i class="far fa-edit"></i></button>
+                        <button id="del-btn"><i class="fas fa-trash"></i></button>
+                        </div>                       
+                        </div> `;
+      taskList.appendChild(taskItem);
+      inputNewTask.value = "";
+  }      
+    }
+    
+   //Listeners to create a new Task 
+  btnNewTask.addEventListener("click", setNewTask);
+  inputNewTask.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+      setNewTask();
+      inputNewTask.value = "";
+    }
+});
